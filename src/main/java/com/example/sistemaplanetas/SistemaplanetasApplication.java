87b9f7078e7a832cf2aaa75a7b27d1701c11fd4a
JuanResk <juanjo_resk@live.com>
2026-01-07 09:28:48 -0300

    Inicio de refactorización de backend

---
+++ src/main/java/com/example/sistemaplanetas/SistemaplanetasApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemaplanetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaplanetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaplanetasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistemaplanetas/SistemaplanetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemaplanetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaplanetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
