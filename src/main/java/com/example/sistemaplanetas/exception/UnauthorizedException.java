19a4c593c96708633b1318dc7e05a2ba206b27d5
JuanResk <juanjo_resk@live.com>
2026-01-09 18:20:12 -0300

    Reinicio

--- src/main/java/com/example/sistemaplanetas/exception/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-package com.example.sistemaplanetas.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
